b994d5d561f4e8fdd3aa6bd1abcc171a70cdb5a1
Julien Marchand <140317895+Nifrael@users.noreply.github.com>
2024-03-04 18:20:59 +0100

    Update tickets_controller.rb
    c'était un ticket pas un player !! :D

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -17,7 +17,7 @@ class TicketsController < ApplicationController
   def create
     @ticket = Ticket.create(ticket_params)
     @ticket.user_id = current_user.id
-    if player.save
+    if ticket.save
       redirect_to ticket_path(@ticket)
     else
       render :new, status: :unprocessable_entity
