d1bfbc24c7f12976ef41ade741cdf97cf72c7aa8
Albert Nikolli <albert_nikolli@icloud.com>
2024-03-07 14:17:04 +0100

    Added the first lines for the geocode

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,11 @@
 class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+    @markers = @tickets.geocoded.map do |ticket|
+      {
+        lat: ticket.latitude,
+        lng: ticket.longitude
+      }
+    end
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,7 @@ class Ticket < ApplicationRecord
 
   enum :status, { pending: 0, ended: 1 }
   enum :type, { upgrade: 0, damage: 1 }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
