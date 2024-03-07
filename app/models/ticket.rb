ad318da214c6725982d479eb19315db278b4598d
Albert Nikolli <albert_nikolli@icloud.com>
2024-03-07 15:37:28 +0100

    Edited the type to category

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,7 +4,7 @@ class Ticket < ApplicationRecord
   has_many :comments
 
   enum :status, { pending: 0, ended: 1 }
-  enum :type, { upgrade: 0, damage: 1 }
+  enum :category, { upgrade: 0, damage: 1 }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
