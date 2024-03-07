a4ec1ed823d6496747098a9073c002bd0d4f2d61
ricardo canas <canasricardo17@gmail.com>
2024-03-07 16:56:10 +0100

    changement de location par adddress sur toutes les pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_182708) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_142553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_182708) do
     t.string "photo"
     t.integer "category"
     t.text "description"
-    t.string "location"
+    t.string "address"
     t.integer "status"
     t.bigint "user_id", null: false
     t.float "latitude"
