cf846440214134899b414b0bafdf3539e8b571f0
ricardo canas <canasricardo17@gmail.com>
2024-03-08 17:26:17 +0100

    création d'une méthode pour voter positif ou négatif et intégration de la méthode dans la show de ticket

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,22 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
+
+  # def create
+  #   @vote = Vote.new(vote_params)
+  #   @vote.user_id = current_user.id
+  #   @vote.ticket_id = params[:id]
+  #   @vote.up_vote = params[:up_vote]
+  #   @vote.save
+  # end
 
   def create
-    @vote = Vote.new(vote_params)
-    @vote.user_id = current_user.id
-    @vote.ticket_id = params[:id]
-    @vote.up_vote = params[:up_vote]
-    @vote.save
+    Vote.vote_by_user(params[:ticket_id], current_user.id, params[:up_vote] == 'true')
+    redirect_to ticket_path(params[:ticket_id]), notice: 'Votre vote a été enregistré.'
   end
 
-  private
+  # private
 
-  def vote_params
-    params.require(:vote).permit(:ticket_id, :user_id, :up_vote)
-  end
+  # def vote_params
+  #   params.require(:vote).permit(:ticket_id, :user_id, :up_vote)
+  # end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,13 @@ class Vote < ApplicationRecord
   belongs_to :ticket
 
   validates :user_id, uniqueness: { scope: :ticket_id, message: "Vous ne pouvez voter qu'une seule fois par ticket." }
+
+  def self.vote_by_user(ticket_id, user_id, up_vote)
+    vote = find_by(ticket_id: ticket_id, user_id: user_id)
+    if vote
+      vote.update(up_vote: up_vote)
+    else
+      create(ticket_id: ticket_id, user_id: user_id, up_vote: up_vote)
+    end
+  end
 end
