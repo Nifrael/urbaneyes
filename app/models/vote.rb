3f84dc22f67ff5ed6bb68b069db0c3beb76683be
ricardo canas <canasricardo17@gmail.com>
2024-03-08 22:47:30 +0100

    correction de l'erreur lors de la création d'un nouveau vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,7 +5,8 @@ class Vote < ApplicationRecord
   validates :user_id, uniqueness: { scope: :ticket_id, message: "Vous ne pouvez voter qu'une seule fois par ticket." }
 
   def self.vote_by_user(ticket_id, user_id, up_vote)
-    vote = find_by(ticket_id: ticket_id, user_id: user_id)
-    vote.update(up_vote: up_vote)
+    vote = find_or_initialize_by(ticket_id: ticket_id, user_id: user_id)
+    vote.up_vote = up_vote
+    vote.save
   end
 end
