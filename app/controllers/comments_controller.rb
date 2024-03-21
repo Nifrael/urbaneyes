2cbdd0d11e24ec101a3183259f8d1923842ba874
Julien Marchand <nifrael66@gmail.com>
2024-03-21 13:08:19 +0100

    Modification 1.0 du controlleur pour les notifications

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     @comment.ticket_id = params[:ticket_id]
     if @comment.save
+      create_notification
       redirect_to ticket_path(id: @comment.ticket.id)
     else
       render :new, status: :unprocessable_entity
@@ -12,6 +13,19 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def create_notification
+    @notification = @comment.notifications.create(
+      hub_id: @comment.ticket.user.hub.id,
+      notifiable_id: @comment.id
+    )
+    HubChannel.broadcast_to(
+      @notification.hub,
+      user_first_name: @comment.user.first_name,
+      user_last_name: @comment.user.last_name,
+      ticket_title: @comment.ticket.title
+    )
+  end
 
   def comment_params
     params.require(:comment).permit(:ticket_id, :user_id, :content)
