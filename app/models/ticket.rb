97c077f043c2ac3d94faec21abe80688d3d42edf
Julien Marchand <nifrael66@gmail.com>
2024-03-27 18:57:37 +0100

    Ajout de notifications pour la prise en charge du ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ApplicationRecord
   has_many :votes
   has_many :comments
   has_many :notifications, as: :notifiable
+  after_update :send_notification
 
   enum status: { pending: 0, ended: 1 }, _prefix: :status
   enum category: { upgrade: 0, damage: 1 }
@@ -25,5 +26,28 @@ class Ticket < ApplicationRecord
   def within_area_of_user?(user)
     results = Geocoder.search(user.address)
     distance_to(results.first.coordinates).round(2) <= 2
+  end
+
+  def send_notification
+    if status == "ended"
+      User.all.each do |user|
+        if within_area_of_user?(user)
+          notification = self.notifications.create(
+            hub_id: user.hub.id,
+            notifiable_id: self.id,
+          )
+          HubChannel.broadcast_to(
+            notification.hub,
+            # user_first_name: @ticket.user.first_name,
+            # user_last_name: @ticket.user.last_name,
+            # ticket_title: @ticket.title,
+            # ticket_id: @ticket.id,
+            # created_at: @notification.created_at,
+            unread_notification: 1
+            # notifiable_type: @notification.notifiable_type
+          )
+        end
+      end
+    end
   end
 end
