f985fc6136e327c67201b48cbf305350dc3de76c
ricardo canas <canasricardo17@gmail.com>
2024-03-27 23:36:05 +0100

    correction des dépendances des models

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :user
-  has_many :votes
-  has_many :comments
+  has_many :votes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :notifications, as: :notifiable
 
   enum status: { pending: 0, ended: 1 }, _prefix: :status
